b962e3e8059de60d3bfeb52b47d778bf7a321b54
Junshen Gao <57498746+Op1a4tzd@users.noreply.github.com>
2020-11-30 11:49:26 -0500

    Search works

--- src/js/index.js
+++ src/js/index.js
@@ -13,39 +13,41 @@ const init = async function () {
       console.log(searchBar);
       searchBar.addEventListener("keyup", function(game) {
         console.log("Yes");
+        if (game.key === 'Enter'){
+          DOMSelectors.grid.insertAdjacentHTML(
+            "beforeend", 
+    
+        `<div class="game-card"> 
+          <div class="game-card-front">
+          </div>
+    
+            <div class="game-card-back">
+            <h3 class="game-card-header">${game.title}</h3>
+            <div class="deals">
+              <p class="best-deal">Best Deal</p>
+              <p class="best-deal">${game.salePrice}</p>
+            </div>
+            </div>
+    
+            <div class="regular-price">
+              <p class="normal-price">Normal Price</p>
+              <p class="normal-price">${game.normalPrice}</p>
+            </div>
+    
+            <div class="rating">
+              <p class="deal-rating">Deal Rating</p>
+              <p class="deal-rating">${game.dealRating}</p>
+            
+          </div>
+            </div>`
+           
+          
         
+          );
+        }
         const searchString = game.target.value; 
       });
-    DOMSelectors.grid.insertAdjacentHTML(
-        "beforeend",
 
-    `<div class="game-card"> 
-      <div class="game-card-front">
-      </div>
-
-        <div class="game-card-back">
-        <h3 class="game-card-header">${game.title}</h3>
-        <div class="deals">
-          <p class="best-deal">Best Deal</p>
-          <p class="best-deal">${game.salePrice}</p>
-        </div>
-        </div>
-
-        <div class="regular-price">
-          <p class="normal-price">Normal Price</p>
-          <p class="normal-price">${game.normalPrice}</p>
-        </div>
-
-        <div class="rating">
-          <p class="deal-rating">Deal Rating</p>
-          <p class="deal-rating">${game.dealRating}</p>
-        
-      </div>
-        </div>`
-       
-      
-    
-      );
    
   } catch (error) {
     console.log(error);
