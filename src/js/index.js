6bf2a430ff48ffff11d53da774a8e508ed4d26db
Junshen Gao <57498746+Op1a4tzd@users.noreply.github.com>
2020-12-01 16:32:57 -0500

    Display works

--- src/js/index.js
+++ src/js/index.js
@@ -14,36 +14,52 @@ const init = async function () {
       searchBar.addEventListener("keyup", function(game) {
         console.log("Yes");
         if (game.key === 'Enter'){
-          DOMSelectors.grid.insertAdjacentHTML(
-            "beforeend", 
-    
-        `<div class="game-card"> 
-          <div class="game-card-front">
-          </div>
-    
-            <div class="game-card-back">
-            <h3 class="game-card-header">${game.title}</h3>
-            <div class="deals">
-              <p class="best-deal">Best Deal</p>
-              <p class="best-deal">${game.salePrice}</p>
-            </div>
-            </div>
-    
-            <div class="regular-price">
-              <p class="normal-price">Normal Price</p>
-              <p class="normal-price">${game.normalPrice}</p>
-            </div>
-    
-            <div class="rating">
-              <p class="deal-rating">Deal Rating</p>
-              <p class="deal-rating">${game.dealRating}</p>
-            
-          </div>
-            </div>`
-           
+          let val = searchBar.value;
+          console.log(val);
           
-        
+          let data2 = data.filter(d =>
+            d.title.toLowerCase().replace(/\s/g, '').includes(val.toLowerCase().replace(/\s/g, ''))      //bruh htis some funy ky
           );
+          console.log(data2);
+
+          data2.forEach(output => {
+            DOMSelectors.grid.insertAdjacentHTML(
+              "beforeend", 
+      
+              `<div class="game-card"> 
+                <div class="game-card-front">
+                </div>
+      
+                <div class="game-card-back">
+                  <img
+                  src= ${output.thumb}
+                  alt=""
+                  class="poster"
+                  />
+                  <h3 class="game-card-header">${output.title}</h3>
+                  <div class="deals">
+                    <p class="best-deal">Best Deal</p>
+                    <p class="best-deal">${output.salePrice}</p>
+                  </div>
+                </div>
+      
+                <div class="regular-price">
+                  <p class="normal-price">Normal Price</p>
+                  <p class="normal-price">${output.normalPrice}</p>
+                </div>
+      
+                <div class="rating">
+                  <p class="deal-rating">Deal Rating</p>
+                  <p class="deal-rating">${output.dealRating}</p>
+              
+                </div>
+              </div>`
+             
+            
+          
+            );
+          });
+          
         }
         const searchString = game.target.value; 
       });
